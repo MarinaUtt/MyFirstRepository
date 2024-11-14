77a5cfab38a1e4c79ca27753c535eacbbfe2a274
MarinaUtt <marinauteshova13@gmail.com>
2024-11-14 10:51:25 +0500

    feat header

---
+++ script.js
@@ -0,0 +1,18 @@
+let burgerMenu = document.querySelector('.burger-menu')
+let mainList = document.querySelector('.main-list')
+
+burgerMenu.addEventListener('click',function(){
+  mainList.classList.toggle('main-list-open')
+  burgerMenu.classList.toggle('open')
+  document.body.classList.toggle('disabled-scroll')
+})
+
+let mainItems = document.querySelectorAll('.main-item')
+
+mainItems.forEach((mainItem) => {
+  mainItem.addEventListener('click', function() {
+    mainList.classList.remove('active')
+    burgerMenu.classList.remove('open')
+    document.body.classList.remove('disabled-scroll')
+  })
+})
